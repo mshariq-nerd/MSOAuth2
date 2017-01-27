bee94f341997fede65a3a683cddf7014dea2a1ad
Mohd Shariq <mohd.shariq@nerdapplabs.com>
2017-01-27 17:35:52 +0530

    Removed unsed code

--- app/src/main/java/com/nerdapplabs/forumapp/activities/ResetPasswordActivity.java
+++ app/src/main/java/com/nerdapplabs/forumapp/activities/ResetPasswordActivity.java
@@ -1,13 +1,19 @@
 package com.nerdapplabs.forumapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.nerdapplabs.forumapp.R;
+import com.nerdapplabs.forumapp.utility.Preferences;
 
-public class ResetPasswordActivity extends AppCompatActivity {
-
+public class ResetPasswordActivity extends AppCompatActivity implements View.OnClickListener{
+    private EditText edtUserName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +22,27 @@ public class ResetPasswordActivity extends AppCompatActivity {
         if (supportActionBar != null) {
             supportActionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        edtUserName = (EditText)findViewById(R.id.edt_username);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            Intent intent = new Intent(getApplicationContext(), LoginActionsActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.btn_reset_password){
+
+        }
+
     }
 }
